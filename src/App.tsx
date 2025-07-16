093fb3cca1a90c6838999e05b62e490fba095da5
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-07-16 16:54:19 -0300

    Clean up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,6 @@
-import { useEffect } from "react";
 import "./App.css";
-import {
-  fakeUsers,
-  fakePersons,
-  fakeCuotas,
-  fakePayments,
-  fakeFamilies,
-  fakeRamas,
-  fakeBalances,
-} from "./mock/fakeData";
-function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("FAKE DATA:", {
-          USERS: fakeUsers,
-          PERSONS: fakePersons,
-          CUOTAS: fakeCuotas,
-          PAYMENTS: fakePayments,
-          FAMILIES: fakeFamilies,
-          RAMAS: fakeRamas,
-          BALANCES: fakeBalances,
-        });
-      } catch (error) {
-        console.log("Error parsing user data:", error);
-      }
-    };
-    fetchData();
-  }, []);
 
+function App() {
   return <>Hello World</>;
 }
 
