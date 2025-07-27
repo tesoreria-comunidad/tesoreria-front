ea76c99e685827ffc0135784f591ba8716230e96
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-07-27 10:26:22 -0300

    Fix build issues

--- src/pages/users/components/table/UsersTable.tsx
+++ src/pages/users/components/table/UsersTable.tsx
@@ -4,7 +4,7 @@ import type { TUser } from "@/models";
 import { useAppSelector } from "@/store/hooks";
 import type { ColumnDef } from "@tanstack/react-table";
 
-export function UsersTable({ usersInput }: { usersInput: TUser[] }) {
+export function UsersTable({ usersInput }: { usersInput?: TUser[] }) {
   const { users } = useAppSelector((s) => s.users);
   const columns: ColumnDef<TUser>[] = [
     {
