15d4521baca8e2da98491db27c8c0d3e0a1c7f47
marianoromero8 <marianoromero97.mr@gmail.com>
2025-07-31 17:56:13 -0300

    feat-familyTable family adapter and api_models changed

--- src/adapters/api_models/family.schema.ts
+++ src/adapters/api_models/family.schema.ts
@@ -4,7 +4,7 @@ import { BaseSchema } from "./baseEntity.schema";
 export const FamilySchema = BaseSchema.extend({
   name: z.string(),
   phone: z.string(),
-  persons: z.array(z.string()), // Array of person IDs
+  users: z.array(z.string()), // Array of person IDs
   id_balance: z.string().optional(), // Optional balance ID
 });
 
--- src/adapters/family.adapter.ts
+++ src/adapters/family.adapter.ts
@@ -2,7 +2,7 @@ import { type TFamily } from "@/models";
 import type { TApiFamily } from "./api_models";
 
 export function familyAdapter(apiFamily: TApiFamily): TFamily {
-  const { createdAt, id, name, persons, phone, updatedAt, id_balance } =
+  const { createdAt, id, name, users, phone, updatedAt, id_balance } =
     apiFamily;
-  return { createdAt, id, name, persons, phone, updatedAt, id_balance };
+  return { createdAt, id, name, users, phone, updatedAt, id_balance };
 }
