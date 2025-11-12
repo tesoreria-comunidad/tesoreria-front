4c5f533c39b34dbf85d2dbb34ba133ad1daeb76c
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2025-11-12 18:50:37 -0300

    Feat:add details to role guard wrapper

--- src/components/guards/RoleGuardWrapper.tsx
+++ src/components/guards/RoleGuardWrapper.tsx
@@ -5,7 +5,7 @@ import type { PropsWithChildren } from "react";
 interface RoleWrapperInterface extends PropsWithChildren {
   roles: TRole[];
 }
-
+// this component render the children if the userRole is included in the list of roles defined in the props
 export function RoleGuardWrapper({ children, roles }: RoleWrapperInterface) {
   const { user } = useAppSelector((s) => s.session);
   if (user?.role && roles.includes(user?.role)) {
